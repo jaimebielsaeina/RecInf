44e0fa98f1f456b65a077c1b464747b0552f8e8e
jaimebielsaeina <116299134+jaimebielsaeina@users.noreply.github.com>
2023-10-25 15:40:33 +0200

    PR3 - Todas las medidas funcionan
    Ya funcionan todas las medidas. Quedan comentadas las funciones que no se han utilizado para tomar las medidas.

--- practica3/evaluation/Evaluation.java
+++ practica3/evaluation/Evaluation.java
@@ -1,7 +1,9 @@
 import java.util.Map;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.TreeMap;
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -21,10 +23,9 @@ public class Evaluation {
 		}
 	}
 
-	public static double AvgPrecision(boolean[] relevantDocuments, Integer K)
+	public static double precision(boolean[] relevantDocuments, Integer K)
 	{
-		if (K <= 0)
-			K = relevantDocuments.length;
+		if (K <= 0 || K > relevantDocuments.length) K = relevantDocuments.length;
 	
 		int numRelevantDocuments = 0;
 	
@@ -34,59 +35,75 @@ public class Evaluation {
 		return (double) numRelevantDocuments / Integer.min(K, relevantDocuments.length);
 	}
 
-	public static double AvgPrecision(boolean[] relevantDocuments) 
+	public static double precision(boolean[] relevantDocuments) 
 	{
-		return AvgPrecision(relevantDocuments, relevantDocuments.length);
+		return precision(relevantDocuments, relevantDocuments.length);
 	}
 
-	public static double MeanAvgPrecision(boolean[][] relevantDocuments, int K) 
+	public static double avgPrecision(boolean[] relevantDocuments, Integer K) 
 	{
-		if (K <= 0) {
+		double avgPrecision = 0.0;
+		int numRelevantDocuments = 0;
+		if (K <= 0 || K > relevantDocuments.length) K = relevantDocuments.length;
+	
+		for (int i = 0; i < K;) 
+		{
+			if (relevantDocuments[i++]) 
+			{
+				numRelevantDocuments++;
+				avgPrecision += (double) numRelevantDocuments / i;
+			}
+		}
+	
+		return (double) avgPrecision / numRelevantDocuments;
+	}
+
+	public static double avgPrecision(boolean[] relevantDocuments) 
+	{
+		return avgPrecision(relevantDocuments, relevantDocuments.length);
+	}
+
+	/*public static double meanAvgPrecision(boolean[][] relevantDocuments, int K) 
+	{
+		if (K <= 0 || K > relevantDocuments.length) {
 			throw new IllegalArgumentException("K must be greater than 0.");
 		}
 	
 		double mapSum = 0.0;
 	
 		for (int i = 0; i < relevantDocuments.length; i++) {
-			mapSum += AvgPrecision(relevantDocuments[i], K);
+			mapSum += precision(relevantDocuments[i], K);
 		}
 	
 		return mapSum / relevantDocuments.length;
-	}
+	}*/
 
-	public static double MeanAvgPrecision(boolean[][] relevantDocuments) 
+	/*public static double meanAvgPrecision(boolean[][] relevantDocuments) 
 	{
 		return MeanAvgPrecision(relevantDocuments, relevantDocuments.length);
-	}
+	}*/
 
-	public static double AvgRecall(boolean[] relevantDocuments, int K, Integer numTotalRelevantDocuments) 
+	public static double recall(boolean[] relevantDocuments, int K, Integer numTotalRelevantDocuments) 
 	{
-		if (K <= 0) {
-			throw new IllegalArgumentException("K must be greater than 0.");
-		}
+		if (K <= 0 || K > relevantDocuments.length) K =	relevantDocuments.length;
 	
 		int numRelevantDocuments = 0;
 	
-		if (numTotalRelevantDocuments == 0) {
-			return 0.0; // Avoid division by zero
-		}
+		if (numTotalRelevantDocuments == 0) return 0.0; // Avoid division by zero
 	
-		for (int i = 0; i < K && i < relevantDocuments.length; i++) 
-		{
-			if (relevantDocuments[i]) {
-				numRelevantDocuments++;
-			}
-		}
+		for (int i = 0; i < K; i++) 
+				if (relevantDocuments[i])
+						numRelevantDocuments++;
 	
 		return (double) numRelevantDocuments / numTotalRelevantDocuments;
 	}
 
-	public static double AvgRecall(boolean[] relevantDocuments, Integer numTotalRelevantDocuments) 
+	public static double recall(boolean[] relevantDocuments, Integer numTotalRelevantDocuments) 
 	{
-		return AvgRecall(relevantDocuments, relevantDocuments.length, numTotalRelevantDocuments);
+		return recall(relevantDocuments, relevantDocuments.length, numTotalRelevantDocuments);
 	}
 
-	public static double MeanAvgRecall(boolean[][] relevantDocuments, int K, Integer numTotalRelevantDocuments) 
+	/*public static double MeanAvgRecall(boolean[][] relevantDocuments, int K, Integer numTotalRelevantDocuments) 
 	{
 		if (K <= 0) {
 			throw new IllegalArgumentException("K must be greater than 0.");
@@ -95,51 +112,38 @@ public class Evaluation {
 		double marSum = 0.0;
 	
 		for (int i = 0; i < relevantDocuments.length; i++) {
-			marSum += AvgRecall(relevantDocuments[i], K, numTotalRelevantDocuments);
+			marSum += recall(relevantDocuments[i], K, numTotalRelevantDocuments);
 		}
 	
 		return marSum / relevantDocuments.length;
-	}
+	}*/
 
-	public static double MeanAvgRecall(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments)
+	/*public static double MeanAvgRecall(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments)
 	{
 		return MeanAvgRecall(relevantDocuments, relevantDocuments.length, numTotalRelevantDocuments);
-	}
-	
+	}*/
 
-	public static double F1Score(boolean[] relevantDocuments, Integer numTotalRelevantDocuments) 
-	{
-		double precision = AvgPrecision(relevantDocuments);
-		double recall = AvgRecall(relevantDocuments, numTotalRelevantDocuments);
-
-		if (precision + recall == 0) {
-			// Handle the case where both precision and recall are zero to avoid division by zero.
-			return 0.0;
-		} else {
-			return 2 * (precision * recall) / (precision + recall);
-		}
-	}
-
-	public static double F1Score(double precision, double recall) 
+	public static double fBettaScore(double betta, double precision, double recall) 
 	{
 		if (precision + recall == 0) {
 			// Handle the case where both precision and recall are zero to avoid division by zero.
 			return 0.0;
 		} else {
-			return 2 * (precision * recall) / (precision + recall);
+			betta *= betta;
+			return (1 + betta) * (precision * recall) / (betta * precision + recall);
 		}
 	}
 
-	public static double MeanF1Score(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments) 
+	/*public static double MeanF1Score(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments) 
 	{
 		double f1Sum = 0.0;
 
 		for (int i = 0; i < relevantDocuments.length; i++) {
-			f1Sum += F1Score(relevantDocuments[i], numTotalRelevantDocuments);
+			f1Sum += fBettaScore(1, relevantDocuments[i], numTotalRelevantDocuments);
 		}
 
 		return f1Sum / relevantDocuments.length;
-	}
+	}*/
 
 	public static ArrayList<PRPoint> computePrecisionRecallCurve(boolean[] relevantDocuments, Integer numTotalRelevantDocuments) 
 	{
@@ -151,18 +155,26 @@ public class Evaluation {
 		{
 			if (relevantDocuments[i]) {
 				numRetrievedDocuments++;
+
+				double precision = (double) numRetrievedDocuments / (i + 1);
+				double recall = (double) numRetrievedDocuments / numTotalRelevantDocuments;
+
+				precisionRecallCurve.add(new PRPoint(recall, precision));
 			}
-
-			double precision = (double) numRetrievedDocuments / (i + 1);
-			double recall = (double) numRetrievedDocuments / numTotalRelevantDocuments;
-
-			precisionRecallCurve.add(new PRPoint(recall, precision));
 		}
 
 		return precisionRecallCurve;
 	}
 
-	public static ArrayList<PRPoint> computeAveragePrecisionRecallCurve(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments) 
+	/*public static ArrayList<PRPoint> interpolatePrecisionRecallCurve(ArrayList<PRPoint> points, int numPoints) {
+		ArrayList<PRPoint> interpolatedCurve = new ArrayList<>();
+
+		for (double i=0; i)
+
+		return interpolatedCurve;
+	}*/
+
+	/*public static ArrayList<PRPoint> computeAveragePrecisionRecallCurve(boolean[][] relevantDocuments, Integer numTotalRelevantDocuments) 
 	{
 		ArrayList<PRPoint> averagePrecisionRecallCurve = new ArrayList<>();
 		int length = relevantDocuments[0].length;
@@ -194,17 +206,18 @@ public class Evaluation {
 		}
 		
 		return averagePrecisionRecallCurve;
-	}
+	}*/
 
-	public static ArrayList<PRPoint> applyStepFunction(ArrayList<PRPoint> precisionRecallPoints) 
+	public static ArrayList<PRPoint> applyStepFunction(ArrayList<PRPoint> precisionRecallPoints, int nPoints) 
 	{
 		// Copy points
-		ArrayList<PRPoint> stepFunctionCurve = new ArrayList<>(precisionRecallPoints); 
+		ArrayList<PRPoint> stepFunctionCurve = new ArrayList<>(precisionRecallPoints);
+		ArrayList<PRPoint> res = new ArrayList<>();
 
 		double maxP = 0;
 
 		// Reverse traverse the array
-		for (int i = stepFunctionCurve.size(); i > 0; i--) 
+		for (int i = stepFunctionCurve.size()-1; i >= 0; i--) 
 		{
 			// If the point is higher than the highest point at the right
 			if (stepFunctionCurve.get(i).precision > maxP) {
@@ -220,10 +233,28 @@ public class Evaluation {
 			}
 		}
 
-		return stepFunctionCurve;
+		double requestedRecall = 1;
+		double lastPrecission = 0;
+		double[] precs = new double[nPoints];
+		int oldCurvePoint = stepFunctionCurve.size()-1;
+		int newCurvePoint = nPoints;
+
+		while (newCurvePoint > 0) {
+			while (oldCurvePoint >= 0 && stepFunctionCurve.get(oldCurvePoint).recall >= requestedRecall - 1e-6) {
+				lastPrecission = stepFunctionCurve.get(oldCurvePoint--).precision;
+			}
+			precs[--newCurvePoint] = lastPrecission;
+			requestedRecall -= 1.0/(nPoints-1);
+		}
+
+		for (int i=0; i<nPoints; i++) {
+			res.add(new PRPoint(1.0*i/(nPoints-1), precs[i]));
+		}
+
+		return res;
 	}
 
-	public static ArrayList<PRPoint> reducePrecisionRecallCurve(ArrayList<PRPoint> curve, int n) 
+	/*public static ArrayList<PRPoint> reducePrecisionRecallCurve(ArrayList<PRPoint> curve, int n) 
 	{
 		if (n <= 0) {
 			throw new IllegalArgumentException("The number of points (n) must be greater than 0.");
@@ -244,11 +275,12 @@ public class Evaluation {
 		}
 	
 		return reducedCurve;
-	}
+	}*/
 
 
 	public static void main (String[] args) {
 
+		// Reads command line arguments.
 		String qRelsFileName = "", resultsFileName = "", outputFileName = "";
 		for (int i = 0; i < args.length; i++) {
 			switch (args[i]) {
@@ -265,7 +297,7 @@ public class Evaluation {
 			System.err.println("java Evaluation -qrels <qrels_file> -results <results_file> -output <output_file>");
 			System.exit(1);
 		}
-		// Read qRels file
+		// Reads qRels file.
 		Map<String, Map<String, Boolean>> qRels = new HashMap<String, Map<String, Boolean>>();
 		try {
 			BufferedReader qRelsFile = new BufferedReader(new FileReader(qRelsFileName));
@@ -273,7 +305,7 @@ public class Evaluation {
 			while ((line = qRelsFile.readLine()) != null) {
 				String[] parts = line.trim().split("[ \t]+");
 				if (!qRels.containsKey(parts[0]))
-						qRels.put(parts[0], new HashMap<String, Boolean>());
+						qRels.put(parts[0], new LinkedHashMap<String, Boolean>());
 				qRels.get(parts[0]).put(parts[1], Integer.parseInt(parts[2]) == 1);
 			}
 			qRelsFile.close();
@@ -282,15 +314,15 @@ public class Evaluation {
 			System.exit(1);
 		}
 		
-		// Read results file
-		Map<String, TreeMap<String, Boolean>> results = new HashMap<String, TreeMap<String, Boolean>>();
+		// Reads results file.
+		Map<String, Map<String, Boolean>> results = new HashMap<String, Map<String, Boolean>>();
 		try {
 			BufferedReader resultsFile = new BufferedReader(new FileReader(resultsFileName));
 			String line;
 			while ((line = resultsFile.readLine()) != null) {
 				String[] parts = line.trim().split("[ \t]+");
 				if (!results.containsKey(parts[0]))
-						results.put(parts[0], new TreeMap<String, Boolean>());
+						results.put(parts[0], new LinkedHashMap<String, Boolean>());
 				results.get(parts[0]).put(parts[1], true);
 			}
 			resultsFile.close();
@@ -299,27 +331,87 @@ public class Evaluation {
 			System.exit(1);
 		}
 
-		for (String qRel : qRels.keySet()) {
-			boolean[] areRelevant = new boolean[results.get(qRel).size()];
-			int i = 0, totalRelevantes = 0;
-			for (String infoNeed : results.get(qRel).keySet()) {
-				areRelevant[i++] = qRels.get(qRel).get(infoNeed);
-				if (qRels.get(qRel).get(infoNeed))
-						totalRelevantes++;
-			}
-			for (i = 0; i < areRelevant.length; i++) {
-				System.out.print(areRelevant[i] ? "1" : "0");
+		try {
+			// Opens output file.
+			PrintWriter outputFile = new PrintWriter(outputFileName);
+			System.out.println("Writing metrics to " + outputFileName + " ...");
+
+			// Defines metrics to be analyzed.
+			double precision, meanPrecision = 0;
+			double recall, meanRecall = 0;
+			double f1, meanF1 = 0;
+			double prec10, meanPrec10 = 0;
+			double avgPrec, map = 0;
+			ArrayList<PRPoint> curve, interpolatedCurve;
+			double[] interpolatedCurvePoints = new double[11];
+
+			// For each information need:
+			for (String qRel : qRels.keySet()) {
+
+				// Gets information about its results.
+				boolean[] areRelevant = new boolean[results.get(qRel).size()];
+				int i = 0, totalRelevantes = 0;
+				for (String infoNeed : results.get(qRel).keySet())
+						areRelevant[i++] = qRels.get(qRel).get(infoNeed);
+				for (Boolean oneIsRel : qRels.get(qRel).values())
+						if (oneIsRel) totalRelevantes++;
+
+				// Computes metrics using the obtained info.
+				precision = precision(areRelevant, 0);
+				recall = recall(areRelevant, 0, totalRelevantes);
+				f1 = fBettaScore(1, precision, recall);
+				prec10 = precision(areRelevant, 10);
+				avgPrec = avgPrecision(areRelevant, 0);
+				curve = computePrecisionRecallCurve(areRelevant, totalRelevantes);
+				interpolatedCurve = applyStepFunction(curve, 11);
+
+				// Updates sum of metrics.
+				meanPrecision += precision;
+				meanRecall += recall;
+				meanF1 += f1;
+				meanPrec10 += prec10;
+				map += avgPrec;
+				for (int j = 0; j < interpolatedCurve.size(); j++)
+						interpolatedCurvePoints[j] += interpolatedCurve.get(j).precision;
+
+				// Prints metrics.
+				outputFile.println("INFORMATION_NEED " + qRel);
+				outputFile.printf("precision %.3f\n", precision);
+				outputFile.printf("recall %.3f\n", recall);
+				outputFile.printf("F1 %.3f\n", f1);
+				outputFile.printf("prec@10 %.3f\n", prec10);
+				outputFile.printf("average_precision %.3f\n", avgPrec);
+				outputFile.println("recall_precision");
+				for (PRPoint point : curve)
+						outputFile.printf("%.3f\t%.3f\n", point.recall, point.precision);
+				outputFile.println("interpolated_recall_precision");
+				for (PRPoint point : interpolatedCurve)
+						outputFile.printf("%.3f\t%.3f\n", point.recall, point.precision);
+				outputFile.println();
+
 			}
 
-			System.out.println();
-			System.out.println("Necesidad de informacion: " + qRel);
-			double precision = AvgPrecision(areRelevant, 0);
-			double recall = AvgRecall(areRelevant, 0, totalRelevantes);
-			System.out.println("precision " + precision);
-			System.out.println("recall " + recall);
-			System.out.println("F1 " + F1Score(precision, recall));
+			// Prints mean metrics.
+			int numInfoNeeds = qRels.size();
+			outputFile.println("TOTAL");	
+			outputFile.printf("precision %.3f\n", meanPrecision / numInfoNeeds);
+			outputFile.printf("recall %.3f\n", meanRecall / numInfoNeeds);
+			outputFile.printf("F1 %.3f\n", meanF1 / numInfoNeeds);
+			outputFile.printf("prec@10 %.3f\n", meanPrec10 / numInfoNeeds);
+			outputFile.printf("MAP %.3f\n", map / numInfoNeeds);
+			outputFile.println("interpolated_recall_precision");
+			for (int i = 0; i < interpolatedCurvePoints.length; i++)
+					outputFile.printf("%.3f\t%.3f\n", 1.0*i/(interpolatedCurvePoints.length-1), interpolatedCurvePoints[i] / numInfoNeeds);
+
+			// Closes output file.
+			outputFile.close();
+
+			System.out.println("Done!");
+
+		} catch (Exception e) {
+			System.err.println("Error: " + e.getMessage());
+			System.exit(1);
 		}
-
 
 	}
 }
